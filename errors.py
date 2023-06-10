757281ed843c4f0437bc8bfc821e39b7c3612b26
eivindtostesen <contact@tostesen.no>
2023-06-10 11:06:29 +0200

    feat: create file errors.py

---
+++ errors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Python module for errors and exceptions.
+
+Created on Thu Mar 30 16:31:58 2023
+
+@author: Eivind Tostesen
+"""
+
+
+class PeakyBlunder(Exception):
+    pass
